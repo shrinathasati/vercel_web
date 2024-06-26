a9dc4b5d92da8bbfd6d089ae40adb91110c46866
shrinathasati <shrinathasati111@gmail.com>
2024-06-26 20:16:38 +0530

    updated backend

--- backend.py
+++ backend.py
@@ -1,80 +1,18 @@
-# from flask import Flask, request, jsonify
-# from pymongo import MongoClient
-# import os
-# # from dotenv import load_dotenv
-# from flask_cors import CORS
-
-# # Load environment variables from .env file
-# # load_dotenv()
-
-# app = Flask(__name__)
-# cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
-
-# client=MongoClient('mongodb+srv://shrinathasati111:sanu2526@cluster0.qogwrwf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-# db=client.get_database('manit')
-# # table=db["men"]
-
-# # table.insert_many([{"image": "https://m.media-amazon.com/images/I/61buPgvuzzL._AC_UY1100_.jpg",
-# #         "details": "Men's product details 1"},{"image": "https://cdn.pixabay.com/photo/2023/05/10/05/56/groom-7983097_640.jpg",
-# #         "details": "Men's product details 2"}])
-
-
-# # Helper function to get collection
-# def get_collection(collection_name):
-#     return db[collection_name]
-
-# @app.route('/products/<collection_name>', methods=['GET'])
-# def get_products_by_collection(collection_name):
-#     collection = get_collection(collection_name)
-#     products = list(collection.find())
-#     for product in products:
-#         product["_id"] = str(product["_id"])
-#     return jsonify(products), 200
-
-# @app.route('/products/search', methods=['GET'])
-# def search_products():
-#     query = request.args.get('q', '')
-#     collections = ['men', 'shirts', 'women']
-#     if query in collections:
-#         collection = get_collection(query)
-#         products = list(collection.find())
-#         for product in products:
-#             product["_id"] = str(product["_id"])
-#         return jsonify(products), 200
-#     # results = []
-
-#     # for collection_name in collections:
-#     #     collection = get_collection(collection_name)
-#     #     products = list(collection.find({
-#     #         "$or": [
-#     #             {"details": {"$regex": query, "$options": "i"}}
-#     #         ]
-#     #     }))
-#     #     for product in products:
-#     #         product["_id"] = str(product["_id"])
-#     #     results.extend(products)
-
-
-#     return jsonify("not found"), 200
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
-from flask_cors import CORS
-from dotenv import load_dotenv
 import os
+# from dotenv import load_dotenv
+from flask_cors import CORS
 
 # Load environment variables from .env file
-load_dotenv()
+# load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-client = MongoClient(os.getenv('MONGO_URI'))
-db = client.get_database('manit')
+
+client=MongoClient('mongodb+srv://shrinathasati111:sanu2526@cluster0.qogwrwf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db=client.get_database('manit')
 
 def get_collection(collection_name):
     return db[collection_name]
@@ -97,7 +35,21 @@ def search_products():
         for product in products:
             product["_id"] = str(product["_id"])
         return jsonify(products), 200
+    # results = []
+
+    # for collection_name in collections:
+    #     collection = get_collection(collection_name)
+    #     products = list(collection.find({
+    #         "$or": [
+    #             {"details": {"$regex": query, "$options": "i"}}
+    #         ]
+    #     }))
+    #     for product in products:
+    #         product["_id"] = str(product["_id"])
+    #     results.extend(products)
+
+
     return jsonify("not found"), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(debug=True)
